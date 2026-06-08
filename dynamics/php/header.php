b22390b2dc63d4dc62771da86fa7f8c914e22bc2
Viridiana Castro Sarabia <324195080@alumno.enp.unam.mx>
2026-06-08 17:51:08 -0600

    style(header): Agrega flex para mejorar diseño responsivo

--- dynamics/php/header.php
+++ dynamics/php/header.php
@@ -1,8 +1,9 @@
 <header>
-    <img class="logo-header" src="/GIT-PUSHERS/statics/media/img/Logo-UNAM.png" alt="Logo UNAM">
-    <img class="logo-header" src="/GIT-PUSHERS/statics/media/img/ETE-logo.png" alt="Logo ETE">
-    <img class="logo-header" src="/GIT-PUSHERS/statics/media/img/P6-logo.png" alt="Logo Prepa 6">
-
+    <div id="logos">
+        <img class="logo-header" src="/GIT-PUSHERS/statics/media/img/Logo-UNAM.png" alt="Logo UNAM">
+        <img class="logo-header" src="/GIT-PUSHERS/statics/media/img/ETE-logo.png" alt="Logo ETE">
+        <img class="logo-header" src="/GIT-PUSHERS/statics/media/img/Logo_ENP6.png" alt="Logo Prepa 6">
+    </div>
     <h1 id="titulo-plataforma">Título Plataforma</h1>
     <h2 id="eslogan">Tu plataforma de desarrollo académico</h2>
 </header>
--- dynamics/php/inicio-sesion.php
+++ dynamics/php/inicio-sesion.php
@@ -11,19 +11,19 @@
 </head>
 <body>
     <?php include 'header.php'; ?>
-    <main class id="contenedor-principal">
+    <main>
         <h2>Bienvenid@<br></h2>
         <section id="tarjeta-loging">
             <h3>Iniciar Sesión</h3>
             <img id="icono-perfil" src="/GIT-PUSHERS/statics/media/img/perfil.png" alt="imagen perfil">
-            <form id="login">
+            <form>
                 <label for="usuario">Nombre de Usuario:</label>
                 <input id="usuario" name="usuario" type="text">
-                <label for="contraseña"><br><br>Contraseña<br></label>
+                <label for="contraseña"><br>Contraseña<br></label>
                 <input id="contraseña" name="contraseña" type="pasword">
                 <p>¿Tienes problemas para iniciar sesión?<br></p>
-                <a href="Equipo_7.php" id= "ayuda"> Ayuda<br></a>
-                <input type="submit" value="Iniciar Sesión">
+                <a id="ayuda" href="Equipo_7.php"> Ayuda<br></a>
+                <input id="boton-inicio-sesion" type="submit" value="Iniciar Sesión">
 
             </form>
         </section>
