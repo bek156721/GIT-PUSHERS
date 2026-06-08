79718fab93457bd8f96802cc04c7db52d4c03c1e
Viridiana Castro Sarabia <324195080@alumno.enp.unam.mx>
2026-06-07 20:54:24 -0600

    feat(login) Se agrega página de inicio de sesión y el encabezado del sitio web

---
+++ dynamics/php/header.php
@@ -0,0 +1,8 @@
+<header>
+    <img class="logo-header" src="/GIT-PUSHERS/statics/media/img/Logo-UNAM.png" alt="Logo UNAM">
+    <img class="logo-header" src="/GIT-PUSHERS/statics/media/img/ETE-logo.png" alt="Logo ETE">
+    <img class="logo-header" src="/GIT-PUSHERS/statics/media/img/P6-logo.png" alt="Logo Prepa 6">
+
+    <h1 id="titulo-plataforma">Título Plataforma</h1>
+    <h2 id="eslogan">Tu plataforma de desarrollo académico</h2>
+</header>
---
+++ dynamics/php/inicio-sesion.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Vridiana Castro">
+    <link rel="stylesheet" href="/GIT-PUSHERS/statics/css/header.css">
+    <link rel="stylesheet" href="/GIT-PUSHERS/statics/css/inicio-sesion.css">
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <main class id="contenedor-principal">
+        <h2>Bienvenid@<br></h2>
+        <section id="tarjeta-loging">
+            <h3>Iniciar Sesión</h3>
+            <img id="icono-perfil" src="/GIT-PUSHERS/statics/media/img/perfil.png" alt="imagen perfil">
+            <form id="loging">
+                <label for="usuario">Nombre de Usuario:</label>
+                <input id="usuario" name="usuario" type="text">
+                <label for="contraseña"><br><br>Contraseña<br></label>
+                <input id="contraseña" name="contraseña" type="pasword">
+                <p>¿Tienes problemas para iniciar sesión?<br></p>
+                <input type="submit" value="Iniciar Sesión">
+
+            </form>
+        </section>
+
+    </main>
+
+</body>
+</html>
