4bf92f4f03e888c45dc1cc43c92183d6472bb57e
Viridiana Castro Sarabia <324195080@alumno.enp.unam.mx>
2026-06-07 23:39:45 -0600

    feat(footer) se agrega archivos de Equipo7 y el footer

--- dynamics/php/footer.php
+++ dynamics/php/footer.php
@@ -1,4 +1,4 @@
 <footer>
-    
+<a href="Equipo_7.php" id="sobre-nosotros"> Sobre Nosotros</a>
 
 </footer>
--- dynamics/php/inicio-sesion.php
+++ dynamics/php/inicio-sesion.php
@@ -7,6 +7,7 @@
     <meta name="author" content="Vridiana Castro">
     <link rel="stylesheet" href="/GIT-PUSHERS/statics/css/header.css">
     <link rel="stylesheet" href="/GIT-PUSHERS/statics/css/inicio-sesion.css">
+    <link rel="stylesheet" href="/GIT-PUSHERS/statics/css/footer.css">
 </head>
 <body>
     <?php include 'header.php'; ?>
@@ -15,18 +16,22 @@
         <section id="tarjeta-loging">
             <h3>Iniciar Sesión</h3>
             <img id="icono-perfil" src="/GIT-PUSHERS/statics/media/img/perfil.png" alt="imagen perfil">
-            <form id="loging">
+            <form id="login">
                 <label for="usuario">Nombre de Usuario:</label>
                 <input id="usuario" name="usuario" type="text">
                 <label for="contraseña"><br><br>Contraseña<br></label>
                 <input id="contraseña" name="contraseña" type="pasword">
                 <p>¿Tienes problemas para iniciar sesión?<br></p>
+                <a href="Equipo_7.php" id= "ayuda"> Ayuda<br></a>
                 <input type="submit" value="Iniciar Sesión">
 
             </form>
         </section>
 
     </main>
+    <footer>
+        <?php include'footer.php';?>
+    </footer>
 
 </body>
 </html>
