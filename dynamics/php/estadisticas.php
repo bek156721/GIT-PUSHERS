7476b24b6827a108c2e7aee5cf0b495ba1d4f6c6
DiegoSerran <324324938@alumno.enp.unam.mx>
2026-06-09 03:00:26 -0600

    feat(estadisticas): agrega plantilla para estadisticas

---
+++ dynamics/php/estadisticas.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Página para consultar estadisticas generales de los estudiantes">
+        <title>Estadisticas</title>
+        <link rel="stylesheet" href="./../../statics/css/graficas.css">
+
+    </head>
+    <body>
+        <header> 
+            <?php ?>
+        </header>
+        
+        <nav>
+            <?php ?>
+        </nav>
+       
+        
+        <main class="contenido-principal">
+            <div class="barra-busqueda">
+                <h1>Estadisticas</h1>
+                <br>
+                <form action="GET" id="form-grupo" >
+                    <span>Grupo: </span>
+                    <select name="consulta" id="grupos">
+                        <!--Se agregaran la lista de grupos según la base de datos-->
+                        <option value="no-seleccionado">Seleccione un grupo</option>
+                    </select>
+                </form>
+            </div>
+            <div id="sesion">
+                <a href="index.php"><img src="https://static.vecteezy.com/system/resources/previews/022/457/896/original/door-icon-adobe-x1-free-vector.jpg" alt=""></a>
+                <a href=""><img src="https://wallpapers.com/images/hd/blank-white-landscape-7sn5o1woonmklx1h.jpg" alt=""></a>
+            </div>
+            <br>
+            <br>
+            <br>
+            <figure>
+                <!--Gráfica de ejemplo, se agregaran según la base de datos-->
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXSAJWC0ds2FLkOksXks8QSNdi8NLjsMq7Ew&s" alt="Gráfica de actividades" class="graficas">
+                <ul class="descripcion-graficas">
+                    <li>Descripción 1</li>
+                    <li>Descripción 2</li>
+                    <li>Descripción 3</li>
+                </ul>
+            </figure>
+            <br>
+            <br>
+            <br>
+            <br>
+            <div class="cambio-página"> 
+                <a href="formularios.php"  id="cambio-formulario">Formularios</a>
+                <a href="actividades.php"  id="cambio-actividades">Actividades</a>
+            </div>
+        </main>
+
+        <footer>
+            <?php ?>
+        </footer>
+    </body>
+</html>
