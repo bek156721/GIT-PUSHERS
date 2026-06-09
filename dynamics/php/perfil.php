551e679142b9a173a8e5d49c0e37a64a28c48992
Rebeca Magallón <bkmagallos@gmail.com>
2026-06-09 13:56:47 -0600

    feat(perfil.php): Cambia foto de perfil

--- dynamics/php/perfil.php
+++ dynamics/php/perfil.php
@@ -1,6 +1,25 @@
 <?php
-?>
+    if(isset($_FILES["foto-perfil"]))
+    {
+        $archivo = $_FILES["foto-perfil"];
+        $ruta_temporal = $archivo["tmp_name"];
 
+        move_uploaded_file($ruta_temporal, "../../statics/media/img/foto-perfil.jpg");
+    }
+    $ruta_imagen = "../../statics/media/img/foto-default.png";
+    if(file_exists("../../statics/media/img/foto-perfil.jpg"))
+    {
+        $ruta_imagen = "../../statics/media/img/foto-perfil.jpg";
+    }
+    else
+    {
+        $ruta_imagen = "../../statics/media/img/foto-default.png";
+    }
+
+    $nombre = "Usuario de prueba";
+    $correo = "correo@actual.com";
+    $numero_cuenta = "123456789";
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -12,13 +31,28 @@
         <link rel="stylesheet" href="../../statics/css/perfil.css">
     </head>
     <body>
-        <h1>Perfil</h1>
-        <p> NOMBRE: $nombre </p>
-        <p> NÚMERO DE CUENTA: $numero_cuenta </p>
-        <p> CORREO: $correo </p>
 
-        <form action = "editar-perfil.php" method = "POST">
-            <button class = "boton" type="submit" class="boton-submit">Editar perfil</button>
-        </form>
+        <nav class = "menu">
+            <button class = "boton-menu"> MIEMBROS </button>
+            <br>
+            <button class = "boton-menu"> MATERIALES </button>
+            <br>
+            <button class = "boton-menu"> ESTADÍSTICAS GRUPALES </button>
+            <br>
+            <button class = "boton-menu"> ALUMNOS INSCRITOS </button>
+        </nav>
+
+        <main class = "contenido-principal">
+            <h1>Perfil</h1>
+            <p> NOMBRE: <?php echo $nombre ?></p>
+            <p> NÚMERO DE CUENTA: <?php echo $numero_cuenta ?></p>
+            <p> CORREO: <?php echo $correo ?></p>
+            <div>
+                <img src = "<?php echo $ruta_imagen; ?>" class = "foto-perfil">
+            </div>
+            <form action = "editar-perfil.php" method = "POST">
+                <button class = "boton" type="submit" class="boton">Editar perfil</button>
+            </form>
+    </main>
     </body>
 </html>
