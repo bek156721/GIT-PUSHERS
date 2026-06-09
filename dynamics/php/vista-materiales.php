e6385fe2e1d8543da3e7b7140fd56de696800225
Diego Lopez Mayen <diegoloma08@gmail.com>
2026-06-09 08:37:05 -0600

    feat(vista-materiales.php): agrega barra de navegacion

---
+++ dynamics/php/vista-materiales.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../../statics/css/vista-materiales.css">
+    <title>Materiales</title>
+</head>
+<body>
+    <div id="cont-ventana">
+        <input type="checkbox" id="menu-toggle" class="menu-checkbox">
+        <header>
+            <div class="navbar">
+                <label for="menu-toggle" class="btn-hamburguesa">☰</label>
+            </div>
+            <h1>Prepa 6</h1>
+        </header>
+        <aside class="menu-lateral">
+            <ul>
+                <li><a href="#">ACTIVIDADES →</a></li>
+                <li><a href="#">FORMULARIO DE EVALUACIÓN →</a></li>
+                <li><a href="#">RECURSOS DE APOYO →</a></li>
+                <li><a href="#">LOL →</a></li>
+                <li><a href="#">PROYECTOS ANTERIORES →</a></li>
+            </ul>
+        </aside>
+        <section id="cont-principal">
+            <div id="cont-circul">
+                <aside id="perfil">
+
+                </aside>
+                <aside id="cerrar-sesion">
+
+                </aside>
+            </div>
+            <h2> MATERIALES </h2>
+            <br>
+            <aside id="cont-materiales">
+                <?php
+                    echo'coco';
+                ?>
+            </aside>
+        </section>
+        <footer>
+            <p>ns que va aquí jsjs</p>
+        </footer>
+    </div>
+    </footer>
+</body>
+</html>
