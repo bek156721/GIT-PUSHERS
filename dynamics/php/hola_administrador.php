7baab898a099935e3be42d7dad512b5cfc2cadf7
Rebeca Magallón <bkmagallos@gmail.com>
2026-06-11 13:22:12 -0600

    feat(hola_administrador): Agrega botón para acceder al perfil del administrador

---
+++ dynamics/php/hola_administrador.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+
+    if ($_SESSION['rol'] != "administrador")
+    {
+        header("Location: inicio-sesion.php");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOLA</title>
+</head>
+<body>
+    <h1>Hola <?php echo $_SESSION['nombre_administrador'] ?><h1>
+    <form action="cerrar-sesion.php" method="POST">
+        <button type="submit">Cerrar sesión</button>
+    </form>
+
+    <form action = "perfil-administrador.php" method = "POST">
+        <button class = "boton" type="submit" class="boton">Perfil</button>
+    </form>
+</body>
+</html>
