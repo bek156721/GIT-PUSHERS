70644419dfd98453c2f00bcdd332de7da6c8e74b
Diego Lopez Mayen <diegoloma08@gmail.com>
2026-06-10 21:44:43 -0600

    feat(vista-materiales.php): agrega tabla de materiales

--- dynamics/php/vista-materiales.php
+++ dynamics/php/vista-materiales.php
@@ -1,3 +1,22 @@
+<?php
+$conexion = mysqli_connect("localhost", "root", "", "sec_ete_db");
+$id_grupo_usuario = 1;
+
+$sql = "SELECT titulo, descripcion, fecha, modulo, url FROM material WHERE id_grupo = $id_grupo_usuario";
+
+$resultado = mysqli_query($conexion, $sql);
+$materiales ;
+$incremento = 0;
+while ($fila = mysqli_fetch_assoc($resultado)) 
+{
+    $materiales[$incremento][0] = $fila['modulo'];
+    $materiales[$incremento][1] = $fila['titulo'];
+    $materiales[$incremento][2] = $fila['descripcion'];
+    $materiales[$incremento][3] = $fila['fecha'];
+    $materiales[$incremento][4] = $fila['url'];
+    $incremento++;
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -36,7 +55,84 @@
             <br>
             <aside id="cont-materiales">
                 <?php
-                    echo'coco';
+                    if ($resultado) 
+                    {
+                        echo '<details id="cont-modul">';
+                        echo '<summary> MODULO 1</summary>';
+                        for ($i=0;$i<$incremento;$i++) 
+                        {
+                            if($materiales[$i][0] == 1)
+                            {
+                                echo '<div id="cont-link">';
+                                echo "<h3>".$materiales[$i][1]."</h3>"; // título
+                                echo "<p>".$materiales[$i][2]."</p>";   // descripción
+                                echo "<p><strong>Fecha:</strong> ".$materiales[$i][3]."</p>"; // fecha
+                                echo "<a href='".$materiales[$i][4]."' target='_blank'>Ver recurso</a>"; // enlace
+                                echo '</div>';
+                            }
+                        }
+                        echo "</details>";
+                        echo '<details id="cont-modul">';
+                        echo '<summary> MODULO 2</summary>';
+                        for ($i=0;$i<$incremento;$i++) 
+                        {
+                            if($materiales[$i][0] == 2)
+                            {
+                                echo '<div id="cont-link">';
+                                echo "<h3>".$materiales[$i][1]."</h3>"; // título
+                                echo "<p>".$materiales[$i][2]."</p>";   // descripción
+                                echo "<p><strong>Fecha:</strong> ".$materiales[$i][3]."</p>"; // fecha
+                                echo "<a href='".$materiales[$i][4]."' target='_blank'>Ver recurso</a>"; // enlace
+                                echo '</div>';
+                            }
+                        }
+                        echo "</details>";
+                        echo '<details id="cont-modul">';
+                        echo '<summary> MODULO 3</summary>';
+                        for ($i=0;$i<$incremento;$i++) 
+                        {
+                            if($materiales[$i][0] == 3)
+                            {
+                                echo '<div id="cont-link">';
+                                echo "<h3>".$materiales[$i][1]."</h3>"; // título
+                                echo "<p>".$materiales[$i][2]."</p>";   // descripción
+                                echo "<p><strong>Fecha:</strong> ".$materiales[$i][3]."</p>"; // fecha
+                                echo "<a href='".$materiales[$i][4]."' target='_blank'>Ver recurso</a>"; // enlace
+                                echo '</div>';
+                            }
+                        }
+                        echo "</details>";
+                        echo '<details id="cont-modul">';
+                        echo '<summary> MODULO 4</summary>';
+                        for ($i=0;$i<$incremento;$i++) 
+                        {
+                            if($materiales[$i][0] == 4)
+                            {
+                                echo '<div id="cont-link">';
+                                echo "<h3>".$materiales[$i][1]."</h3>"; // título
+                                echo "<p>".$materiales[$i][2]."</p>";   // descripción
+                                echo "<p><strong>Fecha:</strong> ".$materiales[$i][3]."</p>"; // fecha
+                                echo "<a href='".$materiales[$i][4]."' target='_blank'>Ver recurso</a>"; // enlace
+                                echo '</div>';
+                            }
+                        }
+                        echo "</details>";
+                        echo '<details id="cont-modul">';
+                        echo '<summary> MODULO 5</summary>';
+                        for ($i=0;$i<$incremento;$i++) 
+                        {
+                            if($materiales[$i][0] == 5)
+                            {
+                                echo '<div id="cont-link">';
+                                echo "<h3>".$materiales[$i][1]."</h3>"; // título
+                                echo "<p>".$materiales[$i][2]."</p>";   // descripción
+                                echo "<p><strong>Fecha:</strong> ".$materiales[$i][3]."</p>"; // fecha
+                                echo "<a href='".$materiales[$i][4]."' target='_blank'>Ver recurso</a>"; // enlace
+                                echo '</div>';
+                            }
+                        }
+                        echo "</details>";
+                    } 
                 ?>
             </aside>
         </section>
