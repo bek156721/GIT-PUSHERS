60d1032c0b425b23006ffb3fd60a020022d3f79a
Viridiana Castro Sarabia <324195080@alumno.enp.unam.mx>
2026-06-15 14:37:39 -0600

    feat(anadir-profesor): Agrega nuevo porfesor a la base de datos

--- dynamics/php/anadir-profesor.php
+++ dynamics/php/anadir-profesor.php
@@ -45,59 +45,59 @@
         $primer_apellido = validate($_POST["primer-apellido"]);
         $segundo_apellido = validate($_POST["segundo-apellido"]);
 
-        $nombre_grupo = $_POST["grupo"];
-        $query_grupo = "SELECT id_grupo FROM grupo WHERE nombre_grupo = '$nombre_grupo'";
-        $res_grupo = mysqli_query($conexion, $query_grupo);
-        $grupo_row = mysqli_fetch_assoc($res_grupo);
-        $grupo = $grupo_row['id_grupo'];
+        //$nombre_grupo = $_POST["grupo"];
+        //$query_grupo = "SELECT id_grupo FROM grupo WHERE nombre_grupo = '$nombre_grupo'";
+        //$res_grupo = mysqli_query($conexion, $query_grupo);
+        //$grupo_row = mysqli_fetch_assoc($res_grupo);
+        //$grupo = $grupo_row['id_grupo'];
 
         //Si los datos obligatorios estan vacios, se redirecciona a la misma página y marca error
 
         if(!password_segura($contrasenia_plana))
         {
-            header("Location: añadir-alumno.php?error=La contraseña debe tener mayúsculas y números");
+            header("Location: anadir-profesor.php?error=La contraseña debe tener mayúsculas y números");
             exit();
         }
         if(empty($usuario))
         {
-            header("Location: añadir-alumno.php?error=Usuario requerido");
+            header("Location: anadir-profesor.php?error=Usuario requerido");
             exit();
         }
         
         if(empty($nombre))
         {
-            header("Location: añadir-alumno.php?error=Nombre requerido");
+            header("Location: anadir-profesor.php?error=Nombre requerido");
             exit();
         }
         if(empty($primer_apellido))
         {
-            header("Location: añadir-alumno.php?error=Primer apellido requerido");
+            header("Location: anadir-profesor.php?error=Primer apellido requerido");
             exit();
         }
 
         //Validar
         if(!filter_var($usuario, FILTER_VALIDATE_INT))
         {
-            header("Location: añadir-alumno.php?error=El usuario debe ser numérico");
+            header("Location: anadir-profesor.php?error=El usuario debe ser numérico");
             exit();
         }
         if(!filter_var($correo, FILTER_VALIDATE_EMAIL))
         {
-            header("Location: añadir-alumno.php?error=EL correo debe ser válido");
+            header("Location: anadir-profesor.php?error=EL correo debe ser válido");
             exit();
         }
 
         $contrasenia = password_hasheada($contrasenia_plana);
-        $query_insertar_alumno = "INSERT INTO alumno (id_alumno, id_grupo, nombre_alumno, primer_apellido_alumno, segundo_apellido_alumno, correo_alumno, contra_alumno ) 
-        VALUES ('$usuario', '$grupo', '$nombre', '$primer_apellido', '$segundo_apellido', '$correo', '$contrasenia')";
-        if(mysqli_query($conexion, $query_insertar_alumno))
+        $query_insertar_profesor = "INSERT INTO profesor (id_profesor, nombre_profesor, primer_apellido_profesor, segundo_apellido_profesor, correo_profesor, contra_profesor ) 
+        VALUES ('$usuario', '$nombre', '$primer_apellido', '$segundo_apellido', '$correo', '$contrasenia')";
+        if(mysqli_query($conexion, $query_insertar_profesor))
             {
-                header("Location: añadir-alumno.php?exito=1");
+                header("Location: anadir-profesor.php?exito=1");
                 exit();
             }
             else
             {
-                header("Location: añadir-alumno.php?exito=0");
+                header("Location: anadir-profesor.php?exito=0");
                 exit();
             }
         
@@ -115,51 +115,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Vridiana Castro">
     <link rel="stylesheet" href="../../statics/css/header.css"> <!-- css de Encabezado -->
-    <link rel="stylesheet" href="../../statics/css/añadir.css"> 
+    <link rel="stylesheet" href="../../statics/css/anadir.css"> 
     <link rel="stylesheet" href="../../statics/css/footer.css"> <!-- css de Pie de página -->
 </head>
 <body>
     <?php include 'header.php'; ?>
     <main>
-        <h2>Añadir Alumno<br></h2>
-        <section id="tarjeta-añadir">
-            <h3>Insertar datos de nuevo alumno</h3>
-            <form action="añadir-alumno.php" method="POST"> <!--Formulario -->
-                <label for="nombre"><br>Nombre del alumno:</label>
-                <input id="nombre" name="nombre" type="text" placeholder="Ingresar el nombre del alumno">
+        <h2>Añadir Profesor<br></h2>
+        <section id="tarjeta-anadir">
+            <h3>Insertar datos de nuevo profesor</h3>
+            <form action="anadir-profesor.php" method="POST"> <!--Formulario -->
+                <label for="nombre"><br>Nombre del profesor:</label>
+                <input id="nombre" name="nombre" type="text" placeholder="Ingresar el nombre del profesor">
 
-                <label for="primer-apellido"><br>Primer apellido del alumno:</label>
+                <label for="primer-apellido"><br>Primer apellido del profesor:</label>
                 <input id="primer-apellido" name="primer-apellido" type="text" placeholder="Ingresa primer apellido">
 
-                <label for="segundo-apellido"><br>Segundo apellido del alumno:</label>
+                <label for="segundo-apellido"><br>Segundo apellido del profesor:</label>
                 <input id="segundo-apellido" name="segundo-apellido" type="text" placeholder="Ingresa segundo apellido">
 
-                <label for="correo"><br>Correo del alumno:</label>
-                <input id="correo" name="correo" type="text" placeholder="Correo del alumno">
+                <label for="correo"><br>Correo del profesor:</label>
+                <input id="correo" name="correo" type="text" placeholder="Correo del profesor">
 
-                <label for="usuario"><br>Usuario del alumno:</label>
-                <input id="usuario" name="usuario" type="text" placeholder="Ingresar el número de cuenta del alumno">
+                <label for="usuario"><br>Usuario del profesor:</label>
+                <input id="usuario" name="usuario" type="text" placeholder="Ingresar el número de trabajador del profesor">
 
                 <label for="contraseña"><br>Contraseña<br></label>
-                <input id="contrasenia" name="contrasenia" type="password" placeholder="Ingresa la contraseña del alumno">
+                <input id="contrasenia" name="contrasenia" type="password" placeholder="Ingresa la contraseña del profesor">
 
-                <label for="grupo"><br>Grupo:</label>
+                <!--<label for="grupo"><br>Grupo:</label>
                 <select id="grupo" name="grupo">
                     <?php
-                    $query_buscar_grupos = "SELECT nombre_grupo FROM grupo";
-                    $res_grupos = mysqli_query($conexion, $query_buscar_grupos);
+                    //$query_buscar_grupos = "SELECT nombre_grupo FROM grupo";
+                    //$res_grupos = mysqli_query($conexion, $query_buscar_grupos);
 
-                    while ($grupo = mysqli_fetch_assoc($res_grupos))
-                    {
-                        echo '<option value="' . $grupo["nombre_grupo"] . '">' . $grupo["nombre_grupo"] . '</option>';
-                    }
+                    //while ($grupo = mysqli_fetch_assoc($res_grupos))
+                    //{
+                      //  echo '<option value="' . $grupo["nombre_grupo"] . '">' . $grupo["nombre_grupo"] . '</option>';
+                    //}
                     ?>
-                </select>
+                </select>-->
 
                 <br>
-                <input id="boton-añadir" type="submit" value="Añadir">
+                <input id="boton-anadir" type="submit" value="Añadir">
             </form>
             <?php 
                 if(isset($_GET['error']))
